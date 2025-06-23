ac933255d043f9676a14b940793fcfab765ee2b5
joselevelsup <joselevelsup@gmail.com>
2025-06-23 15:55:05 -0400

    fix: template

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,58 +1,18 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import supabase from "../client";
+import { Link } from "react-router-dom";
 
 export default function Login(){
-  const [alert, showAlert] = useState({
-    message: "",
-    show: false
-  });
-
-  const navigate = useNavigate();
-
-  const { register, handleSubmit } = useForm({
-    defaultValues: {
-      email: "",
-      password: ""
-    }
-  });
-
-  const loginUser = async (values) => {
-    const { error } = await supabase.auth.signInWithPassword(values);
-
-    if(error){
-      showAlert({
-        show: true,
-        message: error.message
-      }); 
-    } else {
-      navigate("/todos");
-    }
-  }
-
   return (
     <div className="bg-gray-100 flex items-center justify-center min-h-screen">
       <div className="max-w-md w-full bg-white p-6 rounded-lg shadow-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-        {alert.show && 
-          <div className="alert alert-error">
-            <div className="inline-flex justify-stretch items-center">
-              {alert.message}
-              <button onClick={() => showAlert({message: "", show: false})} className="btn btn-ghost btn-circle">
-                X
-              </button>
-            </div> 
-          </div>
-        }
-        <form className="space-y-4" onSubmit={handleSubmit(loginUser)}>
+        <form className="space-y-4">
           <div>
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-            <input id="email" type="email" className="input input-bordered w-full" { ...register("email") } />
+            <input id="email" type="email" className="input input-bordered w-full"/>
           </div>
           <div>
             <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-            <input id="password" type="password" className="input input-bordered w-full" { ...register("password") } />
+            <input id="password" type="password" className="input input-bordered w-full"/>
           </div>
           <button type="submit" className="btn btn-primary w-full">Login</button>
         </form>
