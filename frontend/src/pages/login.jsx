ea2dd303ef0be08428a09a2aea7efe8723e257eb
Tea M <tmelendez@theknowledgehouse.org>
2025-06-30 19:43:38 -0700

    {/* Add LoginForm Here.... */}

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 
-export default function Login(){
+export default function Login() {
+
+
   return (
     <div className="bg-gray-100 flex items-center justify-center min-h-screen">
       <div className="max-w-md w-full bg-white p-6 rounded-lg shadow-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-
+        {/* Add LoginForm Here.... */}
         <p className="mt-4 text-center text-sm">Don't have an account? <Link to="/signup" className="text-blue-500 hover:underline">Sign up</Link></p>
       </div>
     </div>
