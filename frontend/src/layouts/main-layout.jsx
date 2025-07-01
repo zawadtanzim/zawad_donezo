a86cc4904e3d2c58f971b392a147120cf6459217
Tea M <tmelendez@theknowledgehouse.org>
2025-07-01 11:40:23 -0700

    emptying MainLayout - Guide instructions prompt user to add what was originally inside

--- frontend/src/layouts/main-layout.jsx
+++ frontend/src/layouts/main-layout.jsx
@@ -1,19 +1,3 @@
-import { Outlet } from "react-router-dom";
-
 export default function MainLayout(){
-  return (
-    <>
-      <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">Donezo</a>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li><button className="btn btn-link">Logout</button></li>
-          </ul>
-        </div>
-      </div>
-      <Outlet />
-    </>
-  )
+
 }
